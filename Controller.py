d6f07634864cb34956a78b6e378561bbf05c7de1
krittinin <krittin.in@gamil.com>
2017-08-06 20:06:02 +0900

    Initial draft of controller

---
+++ Controller.py
@@ -0,0 +1,20 @@
+# /urs/bin/python
+
+import threading
+import logging
+
+'''
+Main controller to interact with sources and openvim
+'''
+
+# TODO: create thread waiting for sources' request
+
+# TODO: send API to openvim to look up host
+
+# TODO: ssh to host for update info (openvim)?
+
+# TODO: implement algo. of selecting host
+
+# TODO: measure RTT to host
+
+
