f12691382cfc33161bbfcab8aef976736ecb6e30
krittinin <krittin.in@gamil.com>
2017-08-16 14:32:42 +0900

    SSH connection to
    Signed-off-by: krittinin <krittin.in@gamil.com>

--- Client/Source.py
+++ Client/Source.py
@@ -1,13 +1,19 @@
+import random
 import socket
+import threading
 import time
+
+import collections
 import yaml
 import sys
 import sudoku
 import logging
 import os
 import re
+import Queue
 
 max_latency = 2  # second
+
 total_request = 0
 total_reject = 0
 total_error = 0
@@ -18,6 +24,9 @@ logging.basicConfig(level=logging.DEBUG,
                     format=streamformat)
 
 logger = logging.getLogger("Source")
+
+lock = threading.Condition()
+flag_condition = True
 
 
 def read_file(input_file):
@@ -58,13 +67,78 @@ def load_config(config_file):
     return True, config
 
 
-def send_messge(host, port, message):
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect((host, port))
-    sock.sendall(message)
-    response = sock.recv(buffer_size)
-    sock.close()
+def send_message(host, port, message):
+    response = "Error"
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect((host, port))
+        try:
+            sock.sendall(message)
+            response = sock.recv(buffer_size)
+        finally:
+            sock.close()
+    except:
+        logger.error("Socket error " + host, port)
+
     return str(response)
+
+
+def get_server_from_ctrl(max_latency, puzzle, controller_ip, controller_port):
+    ask_msg = str(max_latency) + ',' + str(sys.getsizeof(puzzle))
+    ctrl_rep = send_message(controller_ip, controller_port, ask_msg)
+    ctrl_rep = re.split(',', ctrl_rep)
+    if len(ctrl_rep) != 3:
+        ctrl_rep = {"Error", None, None}
+        logger.error("Controller response error")
+    return ctrl_rep[0], ctrl_rep[1], ctrl_rep[2]
+
+
+class sourceThread(threading.Thread):
+    def __init__(self, cip, cport, d_sip, d_sport, pzzsize, maxL, mode):
+        threading.Thread.__init__(self)
+        self.ctrl_ip = cip
+        self.ctrl_port = cport
+        self.default_server = d_sip
+        self.default_server_port = d_sport
+        self.max_latency = maxL
+        self.puzzle_size = pzzsize
+        self.test_mode = mode == 'test'  # (m == 'test')
+
+    def run(self):
+        puzzle = sudoku.make_puzzle(self.puzzle_size)  # get a sudoku puzzzle
+        if not self.test_mode:
+            accept, server_ip, server_port = get_server_from_ctrl(max_latency=self.max_latency, puzzle=puzzle,
+                                                                  controller_ip=self.ctrl_ip,
+                                                                  controller_port=self.ctrl_port)
+        else:
+            accept, server_ip, server_port = "Accept", self.default_server, self.default_server_port
+            is_reject, is_error = False, False
+
+        if accept == "Reject":
+            is_reject = True
+        elif accept == "Accept":
+            t1 = time.time()
+            solved_puzzle = send_message(server_ip, server_port, str(puzzle))
+            t2 = time.time()
+            print threading.current_thread(), t2 - t1
+            if t2 - t1 > self.max_latency:
+                is_error = True
+
+        global total_request, total_reject, total_error, flag_condition
+        lock.acquire()
+        if flag_condition:
+            flag_condition = False
+            total_request += 1
+            # print total_request
+            if is_reject:
+                total_reject += 1
+            if is_error:
+                total_error += 1
+            flag_condition = True
+            lock.notifyAll()
+        else:
+            lock.wait()
+        lock.release()
 
 
 if __name__ == "__main__":
@@ -76,41 +150,40 @@ if __name__ == "__main__":
 
     logger.setLevel(getattr(logging, config['log_level']))
 
-    controller_ip = config['controller_ip']
-    controller_port = config['controller_port']
+    controller_ip = str(config['controller_ip'])
+    controller_port = int(config['controller_port'])
 
-    puzzle_size = 12
-    server_ip, server_port = '131.112.21.86', 12541
+    puzzle_size = int(config['puzzle_size'])
+    mode = config['mode']
+    is_test_mode = False
+    if mode == 'test':
+        is_test_mode = True
+
+    server_ip = config['default_server_ip']
+    server_port = config['default_server_port']
+
+    wk_rate = 0.09  # per second
+    # server_ip, server_port = '131.112.21.86', 12541
+
+    threads = collections.deque(maxlen=100)
+
     try:
-        while True:
-            puzzle = sudoku.make_puzzle(puzzle_size)
+        for i in range(2):
+            client = sourceThread(controller_ip, controller_port, server_ip, server_port, puzzle_size, max_latency,
+                                  mode)
+            client.daemon = True
+            client.start()
+            threads.append(client)
+            next_workload = random.expovariate(wk_rate)  # as poisson process
+            logger.debug('next workload: ' + str(next_workload))
+            time.sleep(next_workload)
 
-            print 'Puzzle='
-            print puzzle.split()
-
-            # ask controller
-            ask_msg = str(max_latency) + ',' + str(sys.getsizeof(puzzle))
-            ctrl_rep = send_messge(controller_ip, controller_port, ask_msg)
-            total_request += 1
-            ctrl_rep = re.split(',', ctrl_rep)
-            print  ctrl_rep
-            if "Accept" in ctrl_rep:
-                server_ip = ctrl_rep[1]
-                server_port = int(ctrl_rep[2])
-            else:
-                total_reject += 1
-                continue
-            t1 = time.time()
-            solved_puzzle = send_messge(server_ip, server_port, puzzle)
-            print 'Solution='
-            print solved_puzzle.split()
-            del_time = time.time() - t1
-            print 'e2e latency =', del_time
-            if del_time > max_latency:
-                total_error += 1
-            time.sleep(5)
     except KeyboardInterrupt, SystemExit:
         logger.info('Exit the program')
+
+    logger.debug('Wait for all threads')
+    for c in threads:
+        c.join()
 
     print total_request, total_reject, total_error
     exit()
